0d5906b846edf4fcc9c0ad26be6f1c82791b4777
Ahmed Meklad <ahmed.meklad.news@gmail.com>
2018-09-18 22:32:46 +0200

    Project Init

---
+++ README.md
@@ -0,0 +1,8 @@
+# owllog
+
+A new Flutter project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Owllog());
+}
+
+class Owllog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Owllog'),
+        ),
+        body: Card(child: Column(children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text('The News')
+        ],)),
+      ),
+    );
+  }
+}
